b7abde512a07c020642e50f6ed6968f65434324d
Razvan <tangent.jotey@gmail.com>
2021-07-20 23:08:17 +0200

    Update App.svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+  import type { Writable } from 'svelte/store'
+
   import { ProxyWebSocket } from './lib/classes/ProxyWebSocket'
 
   let messages: Array<string> = []
   const websocket: ProxyWebSocket = new ProxyWebSocket(
     `ws://127.0.0.1:8080/chat/${location.hash.substr(1)}`
   )
-  const STATE = websocket.state
+  const STATE: Writable<number> = websocket.state
 
   STATE.subscribe(s => {
     console.log('state:', s)
