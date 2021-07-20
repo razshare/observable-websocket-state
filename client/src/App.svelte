74d1653354caee44dd591f7d09f77a8a73bd2878
Razvan <tangent.jotey@gmail.com>
2021-07-21 00:20:38 +0200

    update

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
-  import type { Writable } from 'svelte/store'
-
-  import { ProxyWebSocket } from './lib/classes/ProxyWebSocket'
-
+  import { derived, readable, writable } from 'svelte/store'
+  import type { Writable, Readable } from 'svelte/store'
   let messages: Array<string> = []
-  const websocket: ProxyWebSocket = new ProxyWebSocket(
-    `ws://127.0.0.1:8080/chat/${location.hash.substr(1)}`
-  )
-  const STATE: Writable<number> = websocket.state
 
-  STATE.subscribe(s => {
-    console.log('state:', s)
-  })
-  websocket.addEventListener('message', e => {
+  const WEBSOCKET: Writable<WebSocket> = writable(
+    new WebSocket(`ws://127.0.0.1:8080/chat/${location.hash.substr(1)}`)
+  )
+
+  const WS: Readable<WebSocket> = derived(WEBSOCKET, $ws => $ws)
+
+
+  /**
+   * This expression triggers only once, at startup.
+   * It doesn't trigger again when the state changes to "closed".
+  */
+  $: ((WS1, WS2) => {
+    console.log("WS1 === WS2?",WS1 === WS2)
+  })($WEBSOCKET, $WS)
+
+  $WEBSOCKET.addEventListener('message', e => {
     messages.push(e.data)
     messages = messages
   })
 </script>
 
-STATE: {$STATE}
+STATE: {$WS.readyState}
 <br />
 Messages
 <br />
